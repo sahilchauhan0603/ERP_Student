50d0568f8a4dcd301293f302736312fcf62718ca
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2025-07-24 16:27:53 +0530

    force reload

--- client/src/pages/studentdashboard/Dashboard.jsx
+++ client/src/pages/studentdashboard/Dashboard.jsx
@@ -224,7 +224,10 @@ const StudentDetailsDashboard = () => {
           <div className="items-center justify-center mt-4">
             <button
               className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors"
-              onClick={() => setShowSuccessModal(false)}
+              onClick={() => {
+                setShowSuccessModal(false);
+                window.location.reload();
+              }}
             >
               Close
             </button>
