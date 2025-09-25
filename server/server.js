5a21e347dcf1b260279e640797d61835bd910f99
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2025-09-25 13:47:04 +0530

    change

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.use('/api/ai', require('./routes/ai'));
 
 // Test route
 app.get('/', (req, res) => {
-  res.send('✅ MySQL API is running...');
+  res.send('✅ MySQL API is running....');
 });
 
 // Start server
